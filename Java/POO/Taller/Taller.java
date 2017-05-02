b7d0c9ea425a2ac1cb3dc55baddc731c5a38bfc0
Iker Domingo <ikerdomingoperez@gmail.com>
2017-05-02 16:51:53 +0200

    Estructura menus alfa

--- Java/POO/Taller/Taller.java
+++ Java/POO/Taller/Taller.java
@@ -46,7 +46,7 @@ public class Taller
     
     public void makeFrame()
     {
-        frame = new JFrame("TUNED");
+        frame = new JFrame("TUNED - Talleres y Tunning UNED");
         makeMenuBar(frame);
 
         //Container contentPane = frame.getContentPane();
@@ -61,24 +61,85 @@ public class Taller
 
     private void makeMenuBar(JFrame frame)
     {
+        final int SHORTCUT_MASK =
+            Toolkit.getDefaultToolkit().getMenuShortcutKeyMask();
+
         JMenuBar menubar = new JMenuBar();
         frame.setJMenuBar(menubar);
 
-        JMenu startMenu = new JMenu("Inicio");
-        menubar.add(startMenu);
-        JMenuItem quitItem = new JMenuItem("Salir");
-            quitItem.addActionListener(e -> quit());
-        startMenu.add(quitItem);
+        JMenu menu;
+        JMenuItem item;
 
-        JMenu userMenu = new JMenu("Usuarios");
-        menubar.add(userMenu);
-        JMenuItem staffUser = new JMenuItem("Plantilla");
-            staffUser.addActionListener(e -> showStaff());
-        userMenu.add(staffUser);
-        JMenuItem customUser = new JMenuItem("Clientes");
-            customUser.addActionListener(e -> showCustom());
-        userMenu.add(customUser);
+        menu = new JMenu("Inicio");
+        menubar.add(menu);
+        item = new JMenuItem("Salir");
+            item.addActionListener(e -> quit());
+        menu.add(item);
 
+        menu = new JMenu("Usuarios");
+        menubar.add(menu);
+        item = new JMenuItem("Plantilla");
+            item.addActionListener(e -> showStaff());
+        menu.add(item);
+        item = new JMenuItem("Clientes");
+            item.addActionListener(e -> showCustom());
+        menu.add(item);
 
+        menu = new JMenu("Promociones");
+        menubar.add(menu);
+        item = new JMenuItem("Editar");
+            item.addActionListener(e -> editPromos());
+        menu.add(item);
+        item = new JMenuItem("Lista");
+            item.addActionListener(e -> listPromos());
+        menu.add(item);
+        item = new JMenuItem("Promociones activas");
+            item.addActionListener(e -> currentPromos());
+        menu.add(item);
+
+        menu = new JMenu("Info");
+        menubar.add(menu);
+        item = new JMenuItem("Estadísticas");
+            item.addActionListener(e -> metrics());
+        menu.add(item);
+
+        menu = new JMenu("About");
+        menubar.add(menu);
+        item = new JMenuItem("About TUNED");
+            item.addActionListener(e -> about());
+        menu.add(item);
+        item = new JMenuItem("Ayuda");
+            item.addActionListener(e -> help());
+        menu.add(item);
+    }
+    private void editPromos() 
+    {
+        JOptionPane.showMessageDialog(frame, "Clientes", "No clientes", 
+                JOptionPane.INFORMATION_MESSAGE);
+    }
+    private void listPromos() 
+    {
+        JOptionPane.showMessageDialog(frame, "Clientes", "No clientes", 
+                JOptionPane.INFORMATION_MESSAGE);
+    }
+    private void currentPromos() 
+    {
+        JOptionPane.showMessageDialog(frame, "Clientes", "No clientes", 
+                JOptionPane.INFORMATION_MESSAGE);
+    }
+    private void metrics() 
+    {
+        JOptionPane.showMessageDialog(frame, "Clientes", "No clientes", 
+                JOptionPane.INFORMATION_MESSAGE);
+    }
+    private void about() 
+    {
+        JOptionPane.showMessageDialog(frame, "Clientes", "No clientes", 
+                JOptionPane.INFORMATION_MESSAGE);
+    }
+    private void help() 
+    {
+        JOptionPane.showMessageDialog(frame, "Clientes", "No clientes", 
+                JOptionPane.INFORMATION_MESSAGE);
     }
 }

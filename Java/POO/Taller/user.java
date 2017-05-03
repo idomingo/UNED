ac4426686f657bcd7364bf54e726e87b571b68ce
iker <iker.domingo.perez@devoteam.com>
2017-05-03 09:34:50 +0200

    initial user staff java files

---
+++ Java/POO/Taller/user.java
@@ -0,0 +1,35 @@
+
+/**
+ * Write a description of class usuario here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class user
+{
+    // instance variables - replace the example below with your own
+    private String name, surname1, surname2;
+    private 
+
+    /**
+     * Constructor for objects of class usuario
+     */
+    public user(String name, String surname1, String surname2, )
+    {
+        this.name = name;
+        this.surname1 = surname1;
+        this.surname2 = surname2;
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     *
+     * @param  y  a sample parameter for a method
+     * @return    the sum of x and y
+     */
+    public int sampleMethod(int y)
+    {
+        // put your code here
+        return x + y;
+    }
+}
